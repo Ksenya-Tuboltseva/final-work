4b0a64b67eb215d4d1ccb292452b92fc7277026d
Ksenya-Tuboltseva <118976448+Ksenya-Tuboltseva@users.noreply.github.com>
2023-01-24 19:32:01 +0300

    Creating a final work

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string [] array = {"2", "no way", "964", "система", "replay", "no"};
+int count = 0;
+for (int i = 0; i < array.Length; i++) //подсчет количества подходящих элементов
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string [] newArray = new string [count];
+int j = 0;
+for (int i = 0; i < array.Length; i++) //заполнение и вывод нового массива
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        Console.Write(newArray[j] + " ");
+        j++;
+    }
+}
